69fcce9916fe98699417b9193f2d325720e31744
Eduard <ed.khusnutdinow@yandex.ru>
2023-08-09 21:06:43 +0500

    Added use cases of application operator.

--- task1.3(Operators).hs
+++ task1.3(Operators).hs
@@ -9,3 +9,12 @@ x |-| y = max (x-y) (y-x)
 
 -- Partial application of an operator
 (`mod` 14) ((+ 5) 10)
+
+-- Operation priority
+func $ x = func x
+
+sin (pi / 2)
+sin $ pi / 2
+-- 
+logBase 4 (min 20 (9 + 7))
+logBase 4 $ min 20 $ 9 + 7
