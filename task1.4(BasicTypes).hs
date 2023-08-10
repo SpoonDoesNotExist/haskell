e05340a7a5d79d73a17ff44f8a4a5fdf14d7bc63
Eduard <ed.khusnutdinow@yandex.ru>
2023-08-10 15:43:19 +0500

    Formatted code

--- task1.4(BasicTypes).hs
+++ task1.4(BasicTypes).hs
@@ -4,12 +4,12 @@ discount :: Double -> Double -> Double -> Double
 discount limit proc sum = if sum >= limit then sum * (100 - proc) / 100 else sum
 
 standardDiscount :: Double -> Double
-standardDiscount = discount 1000 5 
+standardDiscount = discount 1000 5
 
 -- Two digits to Int
 
 twoDigits2Int :: Char -> Char -> Int
-twoDigits2Int x y = if (isDigit x && isDigit y) then 10 * digitToInt x + digitToInt y else 100
+twoDigits2Int x y = if isDigit x && isDigit y then 10 * digitToInt x + digitToInt y else 100
 
 -- 2D points distance
 
@@ -17,4 +17,4 @@ coordinateDiff :: (Double, Double) -> (Double, Double) -> [Double]
 coordinateDiff point1 point2 = [fst point1 - fst point2, snd point1 - snd point2]
 
 dist :: (Double, Double) -> (Double, Double) -> Double
-dist p1 p2 = sqrt $ sum $ map (^2) $ coordinateDiff p1 p2
+dist p1 p2 = sqrt $ sum $ map (^ 2) $ coordinateDiff p1 p2
