6913b024d66737d82fa0855bb53b0f5af1a915b9
Eduard <ed.khusnutdinow@yandex.ru>
2023-08-14 19:19:13 +0500

    TypeClasses

---
+++ task2.3(TypeClasses).hs
@@ -0,0 +1,36 @@
+module TypeClasses where
+
+-- class Eq a where
+--   (==), (/=) :: a -> a -> Bool
+
+-- definition by default
+-- minimal complete definition
+--   x /= y = not (x == y)
+--   x == y = not (x /= y)
+
+-- instance Eq Bool where
+--   True == True = True
+--   False == False = True
+--   _ == _ = False
+
+-- instance (Eq a , Eq b) => Eq (a, b) where
+--     p1 == p2 = fst p1 == fst p2 && snd p1 == snd p2
+--     (/=) p1 p2 = False
+
+-- Printable type class
+
+class Printable a where
+  toString :: a -> String
+
+instance Printable Bool where
+  toString a
+    | a = "true"
+    | otherwise = "false"
+
+instance Printable () where
+  toString a = "unit type"
+
+instance (Printable a, Printable b) => Printable (a, b) where
+  toString (a, b) = "(" ++ toString a ++ "," ++ toString b ++ ")"
+
+--
