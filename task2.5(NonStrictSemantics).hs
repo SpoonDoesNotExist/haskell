9cdbeefbd9e6e85830fb2841a3ff09284ab50ff2
Eduard <ed.khusnutdinow@yandex.ru>
2023-08-22 23:47:32 +0500

    Updated doc for `factorial'`

--- task2.5(NonStrictSemantics).hs
+++ task2.5(NonStrictSemantics).hs
@@ -5,7 +5,7 @@ factorial n = helper 1 n
     helper acc 0 = acc
     helper acc n = helper (acc * n) (n - 1)
 
--- This version explicitly calls calculation of `acc * n` before doing a recursion step
+-- This version explicitly calls calculation of `acc * n` by using `$!` before doing a recursion step
 factorial' :: Integer -> Integer
 factorial' n = helper 1 n
   where
